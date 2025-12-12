574f6b4dcfa13844c2476a781714ea68d6fe1a4d
Mariya <778meri780@gmail.com>
2025-12-12 15:17:56 +0500

    Добавьте файлы проекта.

---
+++ TodoListAPI/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public virtual ICollection<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoListAPI/Models/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoListAPI.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? DueDate { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
